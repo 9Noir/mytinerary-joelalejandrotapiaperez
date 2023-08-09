fe95a57799b31fc9d29880a1358957afc0f40952
Noir <jat_9000@hotmail.com>
2023-08-09 13:11:15 -0300

    Updated home page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,6 @@ export default function NavBar() {
     const navRef = useRef(null);
     const menuRef = useRef(null);
     const location = useLocation();
-    const isHome = location.pathname.startsWith("/home");
 
     function navbarToggle() {
         navRef.current.classList.toggle("max-sm:hidden");
@@ -15,16 +14,16 @@ export default function NavBar() {
     }
 
     return (
-        <header className={`z-10 flex flex-wrap justify-between items-center p-4 w-full font-bold max-w-[1291px] mx-auto ${isHome ? "" : "text-slate-100"}`}>
+        <header className="z-10 flex flex-wrap justify-between items-center p-4 w-full font-bold max-w-[1291px] mx-auto text-slate-100">
             <Anchor to="/home" className="text-3xl text-center">
                 My Tinerary
             </Anchor>
             <button ref={menuRef} onClick={navbarToggle} className="hidden py-1 text-4xl max-sm:flex fa-solid fa-bars"></button>
             <nav ref={navRef} className="flex text-lg [&>*]:p-3 gap-2 max-sm:w-full max-sm:flex-col max-sm:hidden sm:items-center">
-                <Anchor to="/home" className="hover:decoration-blue-500 hover:underline underline-offset-8 decoration-4">
+                <Anchor to="/home" className="hover:decoration-slate-300 hover:underline underline-offset-8 decoration-4">
                     Home
                 </Anchor>
-                <Anchor to="/cities" className="hover:decoration-blue-500 hover:underline underline-offset-8 decoration-4">
+                <Anchor to="/cities" className="hover:decoration-slate-300 hover:underline underline-offset-8 decoration-4">
                     Cities
                 </Anchor>
                 <Button className="px-5" to="/signin">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,23 +4,29 @@ import Carousel from "../components/Carousel";
 Button;
 export default function Home() {
     return (
-        <main className="grow p-4 flex max-w-[1291px] mx-auto">
-            <div className="flex flex-col gap-4 items-center">
-                <div className="text-center max-w-xs space-y-4">
-                    <h1 className="text-3xl font-bold">MyTineraries</h1>
-                    <p className="text-xl italic">"Find your perfect trip, designed by insiders who know and love their cities!".</p>
-                </div>
-                <div className="flex max-md:flex-wrap-reverse gap-6 items-center">
-                    <div className="flex flex-col md:w-1/3 gap-6 xs:gap-12">
-                        <div className="font-bold text-4xl">Find the perfect destination</div>
-                        <div className="text-gray-500 text-2xl">Our app will help you find the perfect path for your next trip. With an easy-to-use interface and a host of itinerary options, planning your next trip has never been easier.</div>
-                        <Button to="/cities" className="max-w-[346px] text-xl">
+        <>
+            <div className="absolute w-full min-h-full bg-cover bg-center bg-[url('./img/home.jpg')] brightness-50"></div>
+            <main className="z-10 grow p-4 flex max-w-[1291px] mx-auto">
+                <div className="flex flex-col gap-4 items-center">
+                    <div className="h-[90vh] flex flex-col justify-center items-center text-slate-100 text-center max-w-md gap-4">
+                        <h1 className="text-5xl font-bold">My Tineraries</h1>
+                        <p className="text-2xl italic font-thin">"Find your perfect trip, designed by insiders who know and love their cities!".</p>
+                        <Button to="/cities" className=" text-xl px-8">
                             Explore now!
                         </Button>
                     </div>
-                    <Carousel />
+                    <div className="flex max-md:flex-wrap-reverse gap-6 items-center">
+                        <div className="flex flex-col md:w-1/3 gap-6 xs:gap-12">
+                            <div className="font-bold text-4xl">Find the perfect destination</div>
+                            <div className="text-gray-500 text-2xl">Our app will help you find the perfect path for your next trip. With an easy-to-use interface and a host of itinerary options, planning your next trip has never been easier.</div>
+                            <Button to="/cities" className="max-w-[346px] text-xl">
+                                View more
+                            </Button>
+                        </div>
+                        <Carousel />
+                    </div>
                 </div>
-            </div>
-        </main>
+            </main>
+        </>
     );
 }
